38020e6a50b2e37fdfc71ab0189e84a20bae08cf
Ilya Zelenin <wyster@make.im>
2020-04-10 10:04:40 +0300

    feat: added dot env

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php declare(strict_types=1);
 
-phpinfo();
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+?>
+Docker container <?=(int)getenv('COMPOSER_INSTALL') ? 'with' : 'without'; ?>  composer packages install
+
+<?=phpinfo(); ?>
